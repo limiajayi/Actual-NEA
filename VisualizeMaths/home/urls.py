471553e77feb429922012460ff90020d7b4837a1
limiajayi <limiajayi@gmail.com>
2023-12-02 20:28:19 +0000

    Cleaned up a few things

--- VisualizeMaths/home/urls.py
+++ VisualizeMaths/home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('login/', views.login, name='login'),
+    path('', views.home, name='home'), #the url pointing to views.home
+    path('login/', views.login, name='login'), 
     path('signup/', views.signup, name='signup'),
 ]
