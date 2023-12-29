d541830a575cb25114c3f6b851d15e85faec3af3
limiajayi <limiajayi@gmail.com>
2023-12-29 16:17:34 +0000

    Defined the point system.

--- VisualizeMaths/students/models.py
+++ VisualizeMaths/students/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from home.models import StudentUser
 
 # Create your models here.
+class MathsPoints(models.Model):
+    """Stores the maths points in each maths topic for each user within StudentUser"""
+    username = models.OneToOneField(StudentUser, on_delete=models.CASCADE, null=True, unique=True)
+    quadratics = models.IntegerField(default=0, blank=True)
+    equations_and_inequalities = models.IntegerField(default=0, blank=True)
+    graphs_and_transformations = models.IntegerField(default=0, blank=True)
+    straight_line_graphs = models.IntegerField(default=0, blank=True)
+    circles = models.IntegerField(default=0, blank=True)
+    trigonometry = models.IntegerField(default=0, blank=True)
+    differentiation = models.IntegerField(default=0, blank=True)
+    integration = models.IntegerField(default=0, blank=True)
+    two_d_vectors = models.IntegerField(default=0, blank=True)
+
+    def __str__(self):
+        return str(self.username)
+    
+class FurtherMathsPoints(models.Model):
+    """Stores the further maths points in each further maths topic for users doing further maths within StudentUser"""
+    username = models.OneToOneField(StudentUser, on_delete=models.CASCADE, null=True, unique=True)
+    differentiation = models.IntegerField(default=0, blank=True)
+    integration = models.IntegerField(default=0, blank=True)
+    argand_diagrams = models.IntegerField(default=0, blank=True)
+    volumes_of_revolution = models.IntegerField(default=0, blank=True)
+    methods_in_calculus = models.IntegerField(default=0, blank=True)
+    matrices = models.IntegerField(default=0, blank=True)
+    polar_coordinates = models.IntegerField(default=0, blank=True)
+    hyperbolic_functions = models.IntegerField(default=0, blank=True)
+    three_d_vectors = models.IntegerField(default=0, blank=True)
+
+    def __str__(self):
+        return str(self.username)
--- VisualizeMaths/students/views.py
+++ VisualizeMaths/students/views.py
@@ -1,19 +1,46 @@
 from django.shortcuts import render, redirect
 from home.models import StudentUser
+from .models import MathsPoints, FurtherMathsPoints
 
 # Create your views here.
 def get_user(request):
     """Returns the session ID corresponding to user"""
     return StudentUser.objects.get(id=request.session['user_id'])
 
+def appendToMaths(request):
+    """Adds users to the MathsPoints table."""
+    user = get_user(request)
+    #If the user is within MathsPoints then do nothing
+    if MathsPoints.objects.filter(username=user).exists():
+        return None
+    #else save them in the table
+    else:
+        new_user = MathsPoints(username=user)
+        new_user.save()
+
+def appendToFurtherMaths(request):
+    """Adds users doing further maths to FurtherMathsPoints table."""
+    user = get_user(request)
+    #If the user is within FurtherMathsPoints then do nothing
+    if FurtherMathsPoints.objects.filter(username=user).exists():
+        return None
+    #Else if the user does further maths and they are not already in the table, save them in the table.
+    elif user.further_maths == True:
+        new_user = FurtherMathsPoints(username=user)
+        new_user.save()
+
 def dash(request):
     """Returns the Student Dash"""
     if 'user_id' in request.session:
         user = get_user(request)
+        #Adds a user to either MathsPoints or FurtherMathsPoints
+        appendToMaths(request)
+        appendToFurtherMaths(request)
         context = {
             'user':user,
             }
         return render(request, 'students/dash.html', context)
+    #If there is no user id in request.session, redirect the user to the sign up page
     else:
         return redirect('/home/signup/')
     
